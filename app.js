5a0e51f42a95823b9cee5b761bcc0c0eed7cdd1f
sonam1975 <mingmar7@gmail.com>
2019-12-30 14:49:07 -0800

    part one completed

--- app.js
+++ app.js
@@ -1,34 +1,67 @@
-const Cucumber = () => {
-  return <li>cucumber</li>
-}
-const Tomatoes = () => {
-  return <li>tomato</li>
-}
+// const Cucumber = () => {
+//   return <li>cucumber</li>
+// }
+// const Tomatoes = () => {
+//   return <li>tomato</li>
+// }
 
 const GroceryList = () => {
   return (
     <div>
-      <Cucumber />
-      <Tomatoes />
       <GroceryListItems items = {['apple', 'banana', 'chocolate']} />
-
     </div>
-
   )
 }
 
+class GroceryListItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
 
-const GroceryListItems = (props) => {
-  return (
-    <ul>
-      <li>{props.items[0]}</li>
-      <li>{props.items[1]}</li>
-      <li>{props.items[2]}</li>
-    </ul>
-  )
+  }
+
+  onListItemsHover() {
+    this.setState({
+
+      done: !this.state.done
+    });
+
+
+
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+    return (
+      <ul>
+        <li style = {style} onMouseEnter={this.onListItemsHover.bind(this)} onMouseLeave={this.onListItemsHover.bind(this)}> {this.props.items[0]}</li>
+        <li>{this.props.items[1]}</li>
+        <li>{this.props.items[2]}</li>
+      </ul>
+    )
+  }
 }
+
 
 ReactDOM.render(
   <GroceryList/>,
   document.getElementById('app')
 )
+
+// const GroceryListItems = (props) => {
+//   var onListItemsHover = (event) => {
+
+//   }
+
+//   return (
+//     <ul>
+//       <li onClick={onListItemsClick}> {props.items[0]}</li>
+//       <li>{props.items[1]}</li>
+//       <li>{props.items[2]}</li>
+//     </ul>
+//   )
+// }
