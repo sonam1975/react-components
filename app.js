f8f13122f4e46c3ee32a0e8be8ebd64267f476b9
sonam1975 <mingmar7@gmail.com>
2019-12-30 11:08:25 -0800

    React components created and rendered

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// TODO
+const Cucumber = () => {
+  return <li>cucumber</li>
+}
+const Tomatoes = () => {
+  return <li>tomato</li>
+}
+
+const GroceryList = () => {
+  return (
+    <div>
+      <Cucumber />
+      <Tomatoes />
+    </div>
+
+  )
+}
+
+ReactDOM.render(
+  <GroceryList />,
+  document.getElementById('app')
+)
